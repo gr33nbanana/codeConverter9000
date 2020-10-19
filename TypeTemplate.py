c6b288754d3842d9da7a02d8d4f106ac3ea1eb92
Kaloyan Pavlov <kaloyan.pavlov@grs.de>
2020-10-19 09:34:54 +0200

    Add comments to functions

--- TypeTemplate.py
+++ TypeTemplate.py
@@ -6,12 +6,14 @@ class TypeTemplate:
     template = [ [False,'!IMPLICIT NONE'], [False,'!INTEGER(4) ::'], [False,'!REAL(8) ::'], [False,'!CHARACTER() ::'] ]
     #Lines template
     def __commentToggle(self, idx):
+        """Toggle if the line at template[idx] is commented out or not"""
         if( self.template[idx][1][0] != "!"):
             self.template[idx][1] ="!" + self.template[idx][1]
         else:
             self.template[idx][1] = self.template[idx][1].replace("!","")
 
     def addVariable(self, varName):
+        """Add a variable to the template list. Case insensitive. Variables are parsed to the proper variable type line by the determineType(varName) method """
         varName = varName.upper()
         #Add variable:
         ##--Add to list of all variables
@@ -27,6 +29,7 @@ class TypeTemplate:
             #self.template[typeIndex][1] = self.template[typeIndex][1].replace("!","")
 
     def removeVariable(self, varName):
+        """Removes the given variable from the template and the list of recognizedVariables. Case insensitive"""
         #Remove variable:
         ##--if it's the last variable, set flag to False
         varName = varName.upper()
@@ -38,6 +41,12 @@ class TypeTemplate:
             #self.template[typeIndex][1] ="!"+self.template[typeIndex][1]
 
     def determineType(self, varName):
+        """Deterines the index in the template list for the given variable.
+        0 - implicit none declaration
+        1 - INTEGER(4)
+        2 - REAL(8)
+        3 - CHARACTER(..)
+        n - others are detected Dimensions """
         #DetermineType
         varName = str(varName).upper()
         if(ord('I')<= ord(varName[0]) <=ord('N')):
@@ -47,20 +56,26 @@ class TypeTemplate:
             #REAL(8)
             return self.realIndex
     def getTemplate(self):
+        """Returns a list of strings contianing the lines for type declaration
+        ['IMPLICIT NONE', 'INTEGER(4) :: var1,var2', ...]"""
         declarationLines = []
         for line in self.template:
             declarationLines.append( line[1] + ", ".join( line[2:] ) )
         return declarationLines
 
     def printTemplate(self):
+        """Prints the lines for type declaration. """
         for line in self.getTemplate():
             print(line)
+
+    ## TODO: DIMENSION PARSER
+    #
 #%%##############################################################
 tst = TypeTemplate()
 tst.template
 #tst.commentToggle(1)
-tst.addVariable("zMax")
-tst.removeVariable("zmax")
+tst.addVariable("kmax")
+tst.removeVariable("kmax")
 tst.printTemplate()
 
 #%%#####
